bf3de3f44f0f1b95a6681de4ae3a345d98f22e83
Luis Felipe Carvalho Arcanjo <pet0160424@aluno.firjansenaisesi.com.br>
2026-04-15 20:14:25 -0300

    primeiro commit (com erro)

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { type Application } from "express"
+import "reflect-metadata"
+import { AppDataSource } from "./data-source"
+import { userRoutes } from "./routes/userRoutes"
+
+const app: Application = express()
+app.use(express.json())
+app.use("/api/users", userRoutes)
+
+AppDataSource.initialize().then(
+    () => {
+        console.log("Banco conectado.")
+        app.listen(process.env.PORT, () => {
+            console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
+        })
+    }
+)
+.catch ((error) => console.log("Erro ao conectar no banco: ", error))
